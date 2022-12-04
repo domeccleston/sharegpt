0a88b74723821bd33afeb090b5df32ed750f3879
Dom Eccleston <domeccleston@gmail.com>
2022-12-04 11:58:40 +0000

    log html

--- pages/api/save.ts
+++ pages/api/save.ts
@@ -17,7 +17,11 @@ export default async function handler(
   if (req.headers.origin !== "https://chat.openai.com")
     return res.status(400).json("Invalid origin");
   const html = req.body;
+  console.log(html);
   const id = uuid();
-  const result = await redis.set(id, html);
-  res.status(200).json({ result });
+  if (req.body) {
+    const result = await redis.set(id, html);
+    res.status(200).json({ result });
+  }
+  return res.status(200).end;
 }
