2a0b90d92245ab21c033c9a00d5d2c809c384e9a
Dom Eccleston <domeccleston@gmail.com>
2022-12-04 11:55:03 +0000

    save html to reids

--- pages/api/save.ts
+++ pages/api/save.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Redis } from "@upstash/redis";
+import { v4 as uuid } from "uuid";
 
 const redis = new Redis({
   url: "https://global-real-gibbon-30346.upstash.io",
@@ -8,9 +9,15 @@ const redis = new Redis({
     "AXaKACQgYTY3YTUwYjMtMWFlZC00ZTBhLWJlNTEtOGJmNmIyOTIwM2U0ZmYzNjVkYjk1NDNjNGVhNTk1MWJmM2NlOTcyYjlhYmQ=",
 });
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const headers = req.headers;
   if (req.headers.origin !== "https://chat.openai.com")
     return res.status(400).json("Invalid origin");
-  res.status(200).json({ headers });
+  const html = req.body;
+  const id = uuid();
+  const result = await redis.set(id, html);
+  res.status(200).json({ result });
 }
