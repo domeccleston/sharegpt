810b24d4d46daa910218de9465e7217dd9d41cf0
Dom Eccleston <domeccleston@gmail.com>
2022-12-04 21:44:41 +0000

    dark mode bg

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-white dark:bg-[#343541]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
