dc1a2a7e9ee24a813361442a0c59224555f9731b
Dom Eccleston <domeccleston@gmail.com>
2022-12-05 14:26:12 +0000

    add vercel footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import cx from "classnames";
 import { Inter, Cormorant_Garamond } from "@next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -18,9 +20,23 @@ const cormorant = Cormorant_Garamond({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={cx(inter.variable, cormorant.variable)}>
+    <main className={cx("bg-gray-50", inter.variable, cormorant.variable)}>
       <Component {...pageProps} />
       <Analytics />
+      <div className="h-[100px] flex items-center justify-center w-full">
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://vercel.com?utm_source=sharegpt&utm_campaign=oss"
+        >
+          <Image
+            width="200"
+            height="100"
+            alt="Vercel Logo"
+            src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
+          />
+        </Link>
+      </div>
     </main>
   );
 }
