d464b0eb8c06bc9ff5206b107c694b9d7579a8ff
Dom Eccleston <domeccleston@gmail.com>
2022-12-05 17:06:35 +0000

    display icon after submit

--- index.js
+++ index.js
@@ -146,7 +146,9 @@ function createBtn() {
     const { id } = await res.json();
     const url = `https://shareg.pt/${id}`;
     window.open(url, "_blank");
-    button.textContent = "Share";
+    button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+  </svg>Share`;
     isRequesting = false;
   });
   return button;
