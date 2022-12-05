cbc7baaeea9fad8e27b306be5899c0c59106a165
Dom Eccleston <domeccleston@gmail.com>
2022-12-05 10:25:01 +0000

    update robots txt

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
       {
         source: "/api/save",
         headers: [
+          {
+            source: "/:slug",
+            headers: [
+              {
+                key: "x-robots-tag",
+                value: "noindex",
+              },
+            ],
+          },
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
