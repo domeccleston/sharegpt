681f83d749aa2f5fc10daec57613d5f1c4a08332
Steven Tey <stevensteel97@gmail.com>
2022-12-05 12:08:30 -0600

    use sharegpt.com instead

--- components/banner.tsx
+++ components/banner.tsx
@@ -5,7 +5,9 @@ export default function Banner() {
   return (
     <Link
       className="fixed bottom-5 inset-x-0 mx-auto w-60 flex space-x-3 items-center justify-center rounded-lg py-3 font-medium text-gray-600 bg-white hover:bg-gray-50 transition-colors duration-75 border border-gray-100 shadow-md"
-      href="/?src=btn-footer"
+      href="https://sharegpt.com"
+      target="_blank"
+      rel="noopener noreferrer"
     >
       <Image
         alt="ShareGPT logo"
