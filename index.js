29eff2967af429f4ac1980703d45148c85fd8f46
Dom Eccleston <domeccleston@gmail.com>
2022-12-05 16:39:35 +0000

    cursor: default

--- index.js
+++ index.js
@@ -76,10 +76,11 @@ function getGravatarSrc(source) {
 
 function createBtn() {
   const button = document.createElement("button");
+  button.classList.add("btn-neutral");
   const svg = createIconSvg();
-  button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>Share`;
+  button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+</svg>Share`;
   button.style.transition = "all 1s ease-in-out";
   button.style.width = "100px";
   button.style.display = "flex";
@@ -101,6 +102,7 @@ function createBtn() {
     console.log(getAvatarImage());
     isRequesting = true;
     button.textContent = "Sharing...";
+    button.style.cursor = "initial";
 
     const threadContainer = document.querySelector(
       "#__next > div > div.flex.flex-1.flex-col.md\\:pl-52.h-full > main > div.Thread__StyledThread-sc-15plnpr-2.crCFRb > div > div > div"
