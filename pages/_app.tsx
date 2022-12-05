974e98043822d05421d164c8ba78be0613199fbc
Steven Tey <stevensteel97@gmail.com>
2022-12-04 18:59:21 -0600

    Added logo, custom font with @next/font, Vercel analytics, framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
-import '../styles/globals.css'
-import '../styles/highlight.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "../styles/highlight.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+import cx from "classnames";
+import { Inter, Cormorant_Garamond } from "@next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const cormorant = Cormorant_Garamond({
+  variable: "--font-cormorant",
+  subsets: ["latin"],
+  weight: "700",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={cx(inter.variable, cormorant.variable)}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
