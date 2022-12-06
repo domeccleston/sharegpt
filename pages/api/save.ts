3c6f241a283b74b16fca523ee724e1a08c233cf3
Steven Tey <stevensteel97@gmail.com>
2022-12-05 18:54:30 -0600

    added rate limiting to /save endpoint

--- pages/api/save.ts
+++ pages/api/save.ts
@@ -2,13 +2,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Redis } from "@upstash/redis";
 import { customAlphabet } from "nanoid";
+import { Ratelimit } from "@upstash/ratelimit";
+
+const redis = Redis.fromEnv();
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+});
 
 const nanoid = customAlphabet(
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   7
 ); // 7-character random string
-
-const redis = Redis.fromEnv();
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,6 +25,10 @@ export default async function handler(
     return res.status(400).json("Invalid origin");
   const html = req.body;
   if (req.method !== "OPTIONS") {
+    const { success } = await ratelimit.limit("sharegpt-save-endpoint");
+    if (!success) {
+      res.status(429).json({ error: "Don't DDoS me pls 🥺" });
+    }
     const id = await setRandomKey(html);
     res.status(200).json({ id });
   } else {
