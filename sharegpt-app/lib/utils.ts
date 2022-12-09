e5ced1e77d4f0837202a3f354759382b443e6396
Steven Tey <stevensteel97@gmail.com>
2022-12-08 23:01:57 -0600

    Added SWR + fetcher

--- sharegpt-app/lib/utils.ts
+++ sharegpt-app/lib/utils.ts
@@ -2,3 +2,26 @@ export const truncate = (str: string, length: number) => {
   if (!str || str.length <= length) return str;
   return `${str.slice(0, length)}...`;
 };
+interface SWRError extends Error {
+  status: number;
+}
+
+export async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init);
+
+  if (!res.ok) {
+    const json = await res.json();
+    if (json.error) {
+      const error = new Error(json.error) as SWRError;
+      error.status = res.status;
+      throw error;
+    } else {
+      throw new Error("An unexpected error occurred");
+    }
+  }
+
+  return res.json();
+}
