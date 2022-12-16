e22807f0d74abfc746b810f89bff0c589567138f
Steven Tey <stevensteel97@gmail.com>
2022-12-16 08:32:15 -0600

    Update popup.js

--- sharegpt-extension/popup.js
+++ sharegpt-extension/popup.js
@@ -1,14 +1,3 @@
-// chrome.runtime.sendMessage({ action: "AUTH_CHECK" }, (session) => {
-//   if (session) {
-//     //user is logged in
-//   } else {
-//     //no session means user not logged in
-//     // chrome.tabs.create({
-//     //   url: "https://sharegpt.com/api/auth/login",
-//     // });
-//   }
-// });
-
 fetch("https://sharegpt.com/api/auth/csrf").then(async (res) => {
   const json = await res.json();
   const csrf = json.csrfToken;
