cf92293b71eb24d644346adc8e89266533b9c50e
Dom <domeccleston@gmail.com>
2023-03-19 20:29:12 +0000

    convert youtube embed to client comp

---
+++ sharegpt-app/components/home/youtube-embed.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+
+export default function YoutubeEmbed() {
+  return (
+    <div className="my-16 px-0 sm:px-2 sm:max-w-screen-lg w-full">
+      <LiteYouTubeEmbed
+        id="lrjC9PTemJw"
+        poster="maxresdefault"
+        title="ShareGPT introduction"
+      />
+    </div>
+  );
+}
